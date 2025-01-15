c6abf95dd2f38a66eea92f64bdbf6b86422a2dc8
Lloyd-Saclot <164666621+lloyd-saclot@users.noreply.github.com>
2025-01-15 13:37:40 -0700

    Updated everything

---
+++ app/week-2/page.js
@@ -0,0 +1,24 @@
+import Title from "./title.js";
+export default function Page() {
+    let a = 68;
+    let b = 1;
+
+    return (
+        <main>
+            <p> This is a ladder</p>
+            < Title />
+            < Title />
+            < Title />
+            < Title />
+            < Title />
+            < Title />
+            < Title />
+            < Title />
+            < Title />
+            <p>SKIBIDI TOILET TWICE</p>
+            <p>
+                The sum of {a} and {b} is {a + b}.
+            </p>
+        </main>
+    );
+}
